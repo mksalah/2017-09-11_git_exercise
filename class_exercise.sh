0bf786c0d3c5dc4a5c6020fbdd058d4d6e29d1f2
Christina La <ckla@usfca.edu>
2017-09-12 20:19:48 -0700

    First commit of working shell script

---
+++ class_exercise.sh
@@ -0,0 +1,20 @@
+# !/bin/bash
+
+# Christina La ckla@usfca.edu
+# September 12, 2017
+
+# Setting up directories
+
+echo "Setting up directories"
+
+mkdir code data data/raw_data output output/figures output/tables
+
+echo "Done making directories"
+
+cd data/raw_data/
+
+echo "Now in raw_data"
+
+curl -L http://npk.io/aMPCOe+ -o filename.fasta
+
+echo "File downloaded"
